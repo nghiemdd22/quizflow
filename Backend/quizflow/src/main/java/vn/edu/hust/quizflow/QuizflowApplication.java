f9e51a3d4b8291096506114c70f233453b643e68
Do Duong Nghiem <doduongnghiem03@gmail.com>
2026-06-03 09:38:04 +0700

    init project

---
+++ Backend/quizflow/src/main/java/vn/edu/hust/quizflow/QuizflowApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hust.quizflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizflowApplication.class, args);
+	}
+
+}
