7bd0eea5a7e1cd45e51fe551055ee7dbc3f7c146
Do Duong Nghiem <doduongnghiem03@gmail.com>
2026-07-23 09:14:55 +0700

    feat: Đưa Web ra trang chủ gốc

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -4,23 +4,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/quizflow/',
   plugins: [
     tailwindcss(), 
     react(),
-    {
-      name: 'redirect-trailing-slash',
-      configureServer(server) {
-        server.middlewares.use((req, res, next) => {
-          if (req.url === '/quizflow') {
-            res.writeHead(301, { Location: '/quizflow/' });
-            res.end();
-          } else {
-            next();
-          }
-        });
-      }
-    }
   ],
   server: {
     proxy: {
